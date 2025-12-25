8ef0a39e8fb83c695ae978d86d69a7276c53a413
Levlink773 <cleandev9@gmail.com>
2025-12-25 21:00:01 +0200

    front 4.6

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useUserStore } from "@/store/useStore";
 import { motion, AnimatePresence } from "framer-motion";
-
+export const dynamic = "force-dynamic";
 // --- ИКОНКИ (Слегка обновленные для стиля) ---
 const Icons = {
     Logo: () => (
